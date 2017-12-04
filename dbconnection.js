f36f13cee540f189f0db20a16f593a5c24231ffc
Han <hjing@dhcp-nyc-797.redrover.cornell.edu>
2017-12-04 16:32:33 -0500

    typo fix

--- dbconnection.js
+++ dbconnection.js
@@ -1,8 +1,8 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
-	host:'localhost'//process.env.MYSQL_DSN || '35.227.62.166' ,
+	host:'localhost',//process.env.MYSQL_DSN || '35.227.62.166' ,
 	user:process.env.MYSQL_USER || 'root',
-	password:''//process.env.MYSQL_PASSWORD || 'abcde12345',
+	password:'',//process.env.MYSQL_PASSWORD || 'abcde12345',
 	database:'lit_development'
 
 });
